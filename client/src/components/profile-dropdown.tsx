2a173f2336ed00df4a6e48a8e6ddf1dca97d2df1
BouchghlAhmedNouh <bouchghlahmed@gmail.com>
2026-01-27 22:34:22 +0100

    fix small bug

--- client/src/components/profile-dropdown.tsx
+++ client/src/components/profile-dropdown.tsx
@@ -23,7 +23,7 @@ export function ProfileDropdown() {
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full ">
             <Avatar className="h-8 w-8 ">
-              <AvatarImage src={`${data?.downloadUrl}1`} alt="@shadcn" onLoadingStatusChange={(status)=>{
+              <AvatarImage src={data?.downloadUrl} alt="@shadcn" onLoadingStatusChange={(status)=>{
                 if(status === "error"){
                   refetch();
                 }
